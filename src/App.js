d6d5eab18cfc270de789d45f83afbf291459a226
Emmah-oo <emmaochonogor715@gmail.com>
2022-08-09 07:22:03 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  
+  const [temp, setTemp] = useState(10)
+  const [tempColor, setTempColor] = useState('cold')
+
+  const increaseTemp = () => {
+    if (temp === 100) return
+    setTemp(temp + 1)
+    tempChange()
+  }
+
+  const decreaseTemp = () => {
+    if (temp === -20) return
+    setTemp(temp - 1)
+    tempChange()
+  }
+
+  const tempChange = () => { 
+    if (temp <= 0) {
+      setTempColor('very-cold')
+    } else if (temp > 0) {
+      setTempColor ('cold')
+    }
+    if (temp >= 30) {
+      setTempColor('hot')
+    } 
+    if (temp >= 50) {
+      setTempColor('very-hot')
+    }
+  }
+
+  return (
+    <div className='container'>
+      <div className= {`temp ${tempColor}`}>
+        {temp}deg C
+      </div>
+      <div className='btns'>
+        <button onClick={decreaseTemp}>-</button>
+        <button onClick={increaseTemp}>+</button>
+      </div>
+    </div>
+  )
+}
+export default App;
